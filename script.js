3bf018385a96b11386bcf4e462636450fe5f8e1a
YogiriTakatouu <106744009+YogiriTakatouu@users.noreply.github.com>
2024-04-17 14:28:35 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,42 @@ function scrollSkill() {
     targetElement.scrollIntoView({ behavior: 'smooth', block: 'center'});
   }
 }
+
+function toggleNav() {
+  var overlay = document.getElementById("myNav");
+  if (overlay.style.width === "100%") {
+    closeNav();
+  } else {
+    openNav();
+  }
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function homeNav() {
+  var targetElement = document.querySelector('.intro');
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'center'});
+    closeNav();
+  }
+}
+function aboutNav() {
+  var targetElement = document.querySelector('.isilorem');
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'center'});
+    closeNav();
+  }
+}
+function skillNav() {
+  var targetElement = document.querySelector('.htmlicon');
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'center'});
+    closeNav();
+  }
+}
